d6a71c704b9b0802a6cfeeb2ce4ed478d7519b60
Ana Clara <clarafag@gmail.com>
2021-11-22 17:38:43 -0300

    Exercicio de Activities, Intents, Fragments e o componente de navegação

---
+++ WordsAppStarter/app/src/main/java/com/example/wordsapp/LetterAdapter.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.wordsapp
+
+import android.content.Intent
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Adapter for the [RecyclerView] in [MainActivity].
+ */
+class LetterAdapter :
+    RecyclerView.Adapter<LetterAdapter.LetterViewHolder>() {
+
+
+    private val list = ('A').rangeTo('Z').toList()
+
+
+    class LetterViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val button = view.findViewById<Button>(R.id.button_item)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LetterViewHolder {
+        val layout = LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.item_view, parent, false)
+
+        layout.accessibilityDelegate = Accessibility
+        return LetterViewHolder(layout)
+    }
+
+
+    override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
+        val item = list.get(position)
+        holder.button.text = item.toString()
+        holder.button.setOnClickListener{
+           val action = LetterListFragmentDirections.actionLetterListFragment2ToWordListFragment2(letter= holder.button.text.toString())
+            holder.view.findNavController().navigate(action)
+        }
+    }
+
+
+    companion object Accessibility : View.AccessibilityDelegate() {
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun onInitializeAccessibilityNodeInfo(
+            host: View?,
+            info: AccessibilityNodeInfo?
+        ) {
+            super.onInitializeAccessibilityNodeInfo(host, info)
+
+            val customString = host?.context?.getString(R.string.look_up_words)
+            val customClick =
+                AccessibilityNodeInfo.AccessibilityAction(
+                    AccessibilityNodeInfo.ACTION_CLICK,
+                    customString
+                )
+            info?.addAction(customClick)
+        }
+    }
+}
---
+++ WordsAppStarter/app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -0,0 +1,56 @@
+package com.example.wordsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.FragmentLetterListBinding
+import com.example.wordsapp.databinding.FragmentWordListBinding
+
+
+class WordListFragment : Fragment() {
+
+    companion object{
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+    private var _binding: FragmentWordListBinding? = null
+    private val binding get ()= _binding
+    private lateinit var  recyclerView: RecyclerView
+    private lateinit var letterId: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let{
+            letterId = it.getString(LETTER).toString()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        _binding = FragmentWordListBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = binding!!.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = WordAdapter(letterId, requireContext())
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
